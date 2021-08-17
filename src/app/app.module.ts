b16c2f2fb80a3f3b6156317ae12ab08400b05c29
Paul Dill <paul@pdill.dev>
2021-08-17 13:39:03 -1000

    Fix error from selected-list.  Just had to delete removed declaration for list-item since that was deleted.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ListTitleComponent } from './list-title/list-title.component';
 import { SelectedListComponent } from './selected-list/selected-list.component';
 import { ListItemComponent } from './list-item/list-item.component';
 import { MainListComponent } from './main-list/main-list.component';
-import { ListServiceService } from './list-service.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListTitleComponent,
     SelectedListComponent,
     ListItemComponent,
     MainListComponent,
